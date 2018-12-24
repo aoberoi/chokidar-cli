e895b7b432bca71855c8725dbd32ec6289cdbc42
Ankur Oberoi <aoberoi@gmail.com>
2018-12-24 04:12:18 -0800

    for sanity check, this should fail

--- test/test-all.js
+++ test/test-all.js
@@ -52,9 +52,8 @@ describe('chokidar-cli', function () {
             const touch = touchCmd + changeFile;
 
             // No quotes needed in glob pattern because node process spawn does no globbing
-            // expectKilledByTimeout(run('node index.js "test/dir/**/*.less" -c "' + touch + '"', timeToRun))
-            //     .then(done, done);
-            run(touch).then(done, done);
+            expectKilledByTimeout(run('node index.js "test/dir/**/*.less" -c "' + touch + '"', timeToRun))
+                .then(done, done);
 
             setTimeout(function afterWatchIsReady() {
                 writeFileSync(lessFile, 'content');
